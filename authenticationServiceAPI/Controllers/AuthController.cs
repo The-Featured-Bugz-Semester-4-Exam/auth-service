373422b97269b26278a361815d67429ce34f1cf0
Jonathan <eaajonhl@students.eaaa.dk>
2023-05-19 09:34:07 +0200

    ItWorks

--- authenticationServiceAPI/Controllers/AuthController.cs
+++ authenticationServiceAPI/Controllers/AuthController.cs
@@ -35,12 +35,9 @@ public class AuthController : ControllerBase
     //Metode til at oprette JWT tokens med brugerdefineret felt.
     private string GenerateJwtToken(string username)
     {
-        var securityKey =
-        new
-        SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Secret"]));
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Secret"]));
         
-        var credentials =
-        new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
         var claims = new[]
         {
         new Claim(ClaimTypes.NameIdentifier, username)
--- authenticationServiceAPI/Startup.sh
+++ authenticationServiceAPI/Startup.sh
@@ -9,9 +9,7 @@ export Issuer="myIssuerIsAnIssue"
 #export collection="authCol"
 #echo $database connectionString
 #dotnet run server="$server" port="$port" export connectionString"mongodb://localhost:27017" export database="Auth"
-export mySecret="$mySecret" 
-export myIssuer="$myIssuer"
-dotnet run Issuer="$myIssuer" Secret="$mySecret"
+dotnet run Issuer="$Issuer" Secret="$Secret"
 
 
 
